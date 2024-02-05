29e720309bc527f64da2e6b419c3385047c0b748
pruevangelista <ayessa.evangelista@obf.ateneo.edu>
2024-02-05 12:07:42 +0800

    Modified lar's ver [w/ client, dr number and date created]

--- g_uno_demo/g_uno_app/models.py
+++ g_uno_demo/g_uno_app/models.py
@@ -74,6 +74,7 @@ class DeliveryReceipt(models.Model):
     ]
 
     dr_terms = models.CharField(max_length=3, choices=terms, null=False)
+    client = models.ForeignKey("Client", on_delete=models.CASCADE, blank=True, null=False) #PRUE: Added this.
     product_id = models.ManyToManyField(Product, through=QuantityOrdered, blank=True, null=False)
 
     dr_amt_wo_vat = models.FloatField(null=False, blank=True, default = 0)
@@ -83,10 +84,10 @@ class DeliveryReceipt(models.Model):
         return self.dr_number
     
     def getDrAmtVat(self):
-        return self.dr_amt_vat
+        return "{:,.2f}".format(self.dr_amt_vat)
 
     def getDrAmtWoVat(self):
-        return self.dr_amt_wo_vat
+        return "{:,.2f}".format(self.dr_amt_wo_vat)
     
     def getDate(self):
         return self.dr_date
@@ -103,32 +104,27 @@ class DeliveryReceipt(models.Model):
 
     def __str__(self):
         return (
-            f"DR Number: {self.dr_number}, "
+            f"DR Number: {self.dr_number}"
         )
-     
-
-
     
-
-
 #Client
 class Client(models.Model):
-    client_ID = models.IntegerField(validators=[MaxValueValidator(999999)], null=False, primary_key=True) #Random
+    client_ID = models.AutoField(primary_key=True) #Prue: Changed from IntegerField to AutoField. 
     client_name = models.CharField(max_length=55)
     client_address = models.CharField(max_length=255)
     client_TIN = models.CharField(max_length=15)
     objects = models.Manager()
 
-    def clientID(self):
+    def getClientID(self):
         return self.client_ID
     
-    def clientName(self):
+    def getClientName(self):
         return self.client_name
     
-    def clientAddress(self):
+    def getClientAddress(self):
         return self.client_address
     
-    def clientTIN(self):
+    def getClientTIN(self):
         return self.client_TIN
     
     def __str__(self):
