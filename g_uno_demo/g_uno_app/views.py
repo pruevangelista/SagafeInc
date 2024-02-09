f01fbbb04ae5a961216f7c74c2aab302e8cfbade
Paomittsu <philip.figueroa@obf.ateneo.edu>
2024-02-09 20:19:11 +0800

    Cleaned Views a bit

--- g_uno_demo/g_uno_app/views.py
+++ g_uno_demo/g_uno_app/views.py
@@ -14,25 +14,6 @@ def align_test(request):
 def dr_home(request):
     return render(request, 'g_uno_app/dr_home.html')
 
-def add_dr(request):
-    # Checks if the latest DR instance exists and retrieves its dr_number
-    if DeliveryReceipt.objects.exists(): 
-        latest_DRinstance = DeliveryReceipt.objects.latest('dr_number')
-        latest_DRnumber = latest_DRinstance.dr_number 
-        new_DRnumber = latest_DRnumber + 1 
-    else: 
-        new_DRnumber = 1
-
-    # Saves new_DRnumber only in memory until user actually clicks the confirm button in add_dr.html 
-    dr_instance = DeliveryReceipt(dr_number=new_DRnumber)
-
-    context = {
-        'c': dr_instance,
-        #'d': client_instance, (for suggested client_name inputs)
-        #'e': product_instance, (for suggested product_name inputs)
-    }
-    return render(request, 'g_uno_app/add_dr.html', context)
-
 def new_dr(request):
     if(request.method == "POST"):
         # Client 
@@ -58,7 +39,8 @@ def new_dr(request):
 
         if created: 
             client_instance.save() 
-
+        
+        # Add products to DR
         dr_instance = DeliveryReceipt.objects.create(dr_terms=inp_terms, client=client_instance)
         dr_instance.dr_due_date = dr_instance.dr_date + timedelta(days=int(inp_terms))
 
@@ -72,9 +54,6 @@ def new_dr(request):
             dr_instance.product_id.add(chosen_product)
             dr_instance.save()
             
-
-
-
         dr_instance.dr_amt_wo_vat = sum(gen_subtotal)
         dr_instance.dr_amt_vat = round(sum(gen_subtotal) * Decimal(1.12), 2)
         dr_instance.save()
